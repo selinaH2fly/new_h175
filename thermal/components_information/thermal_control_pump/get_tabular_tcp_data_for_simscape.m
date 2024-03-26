1976b5be2a2387616c7eadab54faa015c3b4ec9d
Stefan Notter <stefan.notter@h2fly.de>
2024-03-26 10:56:23 +0100

    artificial pump values for 1400 rmp discarded again since they appear to yield an unrealistically low pressure rise

--- thermal/components_information/thermal_control_pump/get_tabular_tcp_data_for_simscape.m
+++ thermal/components_information/thermal_control_pump/get_tabular_tcp_data_for_simscape.m
@@ -1,10 +1,14 @@
 function [capacity_vector_lpm, shaft_speed_vector_rpm, head_table_m] = ...
     get_tabular_tcp_data_for_simscape(reference_density, make_plot)
 
-    [pump_info_datasheet, pump_info_ann_prediction] = get_pump_data();
+    % [pump_info_datasheet, pump_info_ann_prediction] = get_pump_data();
+    [pump_info_datasheet, ~] = get_pump_data();
+
     
     capacity_vector_lpm = [10, 20, 30, 40, 50, 75, 100, 150, 200, 250, 300, 350, 400];
-    shaft_speed_vector_rpm = [1400, 2000, 2500, 3000, 3500, 4000, 4200];
+    % shaft_speed_vector_rpm = [1400, 2000, 2500, 3000, 3500, 4000, 4200];
+    shaft_speed_vector_rpm = [2000, 2500, 3000, 3500, 4000, 4200];
+
     
     head_table_kPa = nan(length(capacity_vector_lpm), ...
         length(shaft_speed_vector_rpm));
@@ -22,24 +26,29 @@ function [capacity_vector_lpm, shaft_speed_vector_rpm, head_table_m] = ...
         datasheet_capacity_samples = squeeze(pump_info_datasheet(1,shaft_speed,:,1));
         datasheet_capacity_samples = datasheet_capacity_samples(~isnan(datasheet_capacity_samples));
     
-        head_table_kPa(:, shaft_speed+1) = interp1(datasheet_capacity_samples, ...
+        % head_table_kPa(:, shaft_speed+1) = interp1(datasheet_capacity_samples, ...
+        %     datasheet_head_samples, capacity_vector_lpm);
+
+        head_table_kPa(:, shaft_speed) = interp1(datasheet_capacity_samples, ...
             datasheet_head_samples, capacity_vector_lpm);
 
         if make_plot == true    
-            plot(capacity_vector_lpm, head_table_kPa(:, shaft_speed+1), 'x')
+            % plot(capacity_vector_lpm, head_table_kPa(:, shaft_speed+1), 'x')
+            plot(capacity_vector_lpm, head_table_kPa(:, shaft_speed), 'x')
+
         end
     
     end
 
-    % interpolation of ann prediction values and assignment to lookup table
-    ann_head_samples = pump_info_ann_prediction.rpm1400_degC60(:, 2);
-    ann_capacity_samples = pump_info_ann_prediction.rpm1400_degC60(:, 1);
-    head_table_kPa(:, 1) = interp1(ann_capacity_samples, ...
-            ann_head_samples, capacity_vector_lpm);
-
-    if make_plot == true    
-            plot(capacity_vector_lpm, head_table_kPa(:, 1), 'x')
-    end
+    % % interpolation of ann prediction values and assignment to lookup table
+    % ann_head_samples = pump_info_ann_prediction.rpm1400_degC60(:, 2);
+    % ann_capacity_samples = pump_info_ann_prediction.rpm1400_degC60(:, 1);
+    % head_table_kPa(:, 1) = interp1(ann_capacity_samples, ...
+    %         ann_head_samples, capacity_vector_lpm);
+    % 
+    % if make_plot == true    
+    %         plot(capacity_vector_lpm, head_table_kPa(:, 1), 'x')
+    % end
 
     % conversion to head in terms of meters
     head_table_m = head_table_kPa*1000/(reference_density*9.81);
