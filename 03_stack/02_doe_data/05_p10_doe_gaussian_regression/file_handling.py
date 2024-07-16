c82963f6091e05eb749d650c3ba0833e22bd3068
Stefan Notter <stefan.notter@h2fly.de>
2024-07-16 13:00:03 +0200

    chore: file handling moved to separate file

---
+++ 03_stack/02_doe_data/05_p10_doe_gaussian_regression/file_handling.py
@@ -0,0 +1,55 @@
+import os
+import shutil
+
+# Create and browse to folder for storing experiment results
+def create_experiment_folder(_params_model=None, _params_training=None, _params_logging=None):
+    """ Creates new folder to store training results
+
+    Parameters
+    ----------
+    _params_training : LearningParameters
+        Hyperparameters for training the model
+
+    _params_logging : LoggingParameters
+        Intervals for saving and logging
+
+    Returns
+    -------
+    parameterFile : io.TextIOWrapper
+        File descriptor to parameter file
+
+    lossFile : io.TextIOWrapper
+        File descriptor to loss file
+    """
+
+    # create folder to store data for the experiment running
+    experimentID = 1
+    dirName = "{}_gpr_doe_model_experiment".format(experimentID)
+    while os.path.exists(dirName):
+        experimentID += 1
+        dirName = "{}_".format(experimentID) + dirName.split('_', 1)[1]
+    os.mkdir(dirName)
+    shutil.copytree(os.getcwd(), os.path.join(dirName, "Sources_unzipped"),
+                    ignore=shutil.ignore_patterns('*experiment*', 'archive', '.git*', 
+                                                  '../.idea', '__pycache__', 'README*'))
+
+    os.chdir(dirName)
+    shutil.make_archive("Sources", 'zip', "Sources_unzipped")
+    shutil.rmtree("Sources_unzipped")
+    print("Directory for running experiment no. {} created\n".format(experimentID))
+
+    # save parameters to file
+    parameterFile = open("parameterFile.txt", "w")
+    # write paramters to file if they are not None
+    if _params_model is not None:
+        parameterFile.write(format(vars(_params_model)) + "\n")
+    if _params_training is not None:
+        parameterFile.write(format(vars(_params_training)) + "\n")
+    if _params_logging is not None:
+        parameterFile.write(format(vars(_params_logging)))
+    parameterFile.close()
+
+    # create folder for storing model snapshots
+    os.mkdir("model_performance_snapshots")
+
+    return None
--- 03_stack/02_doe_data/05_p10_doe_gaussian_regression/fit_gaussian_process_model.py
+++ 03_stack/02_doe_data/05_p10_doe_gaussian_regression/fit_gaussian_process_model.py
@@ -24,6 +24,9 @@ import cv2
 import warnings
 from gpytorch.utils.warnings import GPInputWarning
 
+# Import the function to create the experiment folder
+from file_handling import create_experiment_folder
+
 # Import the Gaussian process regression model
 from gpr_model import ExactGPModel
 
@@ -36,60 +39,6 @@ from data_processing import load_high_amp_doe_data, preprocess_data
 # Import input optimization function from input_optimization.py
 from input_optimization import optimize_inputs_evolutionary
 
-
-
-# Create and browse to folder for storing experiment results
-def create_experiment_folder(_params_model=None, _params_training=None, _params_logging=None):
-    """ Creates new folder to store training results
-
-    Parameters
-    ----------
-    _params_training : LearningParameters
-        Hyperparameters for training the model
-
-    _params_logging : LoggingParameters
-        Intervals for saving and logging
-
-    Returns
-    -------
-    parameterFile : io.TextIOWrapper
-        File descriptor to parameter file
-
-    lossFile : io.TextIOWrapper
-        File descriptor to loss file
-    """
-
-    # create folder to store data for the experiment running
-    experimentID = 1
-    dirName = "{}_gpr_doe_model_experiment".format(experimentID)
-    while os.path.exists(dirName):
-        experimentID += 1
-        dirName = "{}_".format(experimentID) + dirName.split('_', 1)[1]
-    os.mkdir(dirName)
-    shutil.copytree(os.getcwd(), os.path.join(dirName, "Sources_unzipped"),
-                    ignore=shutil.ignore_patterns('*experiment*', 'archive', '.git*', 
-                                                  '../.idea', '__pycache__', 'README*'))
-
-    os.chdir(dirName)
-    shutil.make_archive("Sources", 'zip', "Sources_unzipped")
-    shutil.rmtree("Sources_unzipped")
-    print("Directory for running experiment no. {} created\n".format(experimentID))
-
-    # save parameters to file
-    parameterFile = open("parameterFile.txt", "w")
-    # write paramters to file if they are not None
-    if _params_model is not None:
-        parameterFile.write(format(vars(_params_model)) + "\n")
-    if _params_training is not None:
-        parameterFile.write(format(vars(_params_training)) + "\n")
-    if _params_logging is not None:
-        parameterFile.write(format(vars(_params_logging)))
-    parameterFile.close()
-
-    # create folder for storing model snapshots
-    os.mkdir("model_performance_snapshots")
-
-    return None
 
 # Computation of partial dependence
 def partial_dependence(model, X, feature_index, fixed_feature_index=None, fixed_value=None, grid_resolution=100):
