27a930b3c993a78ae1667245c96d517df8426e7d
Stefan Notter <stefan.notter@h2fly.de>
2024-07-18 14:50:55 +0200

    feat: passing the flightlevel to the main function as an argument

--- 03_stack/02_doe_data/05_p10_doe_gaussian_regression/main.py
+++ 03_stack/02_doe_data/05_p10_doe_gaussian_regression/main.py
@@ -3,7 +3,8 @@
 import subprocess
 import argparse
 
-def main(power_constraint_kW, specified_cell_count):
+def main(power_constraint_kW, specified_cell_count, flight_level_100ft):
+
     # Define arguments for training
     target = 'eta_lhv'
     cutoff = 50.0
@@ -16,13 +17,16 @@ def main(power_constraint_kW, specified_cell_count):
     model_path = f'gpr_model_{target}.pth'
 
     # Call the optimization script
-    subprocess.run(['python', 'optimize_input_variables.py', '-m', model_path, '-p', str(power_constraint_kW), '-n', str(specified_cell_count)])
+    subprocess.run(['python', 'optimize_input_variables.py', '-m', model_path, '-p', str(power_constraint_kW), '-n', \
+                    str(specified_cell_count), '-f', str(flight_level_100ft)])
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser(description="Train and optimize a Gaussian process regression model on Powercell DoE data")
     parser.add_argument("-p", "--power", type=float, help="Power constraint for input variable optimization", default=75.0)
     parser.add_argument("-n", "--cellcount", type=int, help="Stack cell number for optimizing subject to power constraint", default=275)
+    parser.add_argument("-f", "--flightlevel", type=int, help="Flight level in 100x feets", default=50)
 
     args = parser.parse_args()
 
-    main(args.power, args.cellcount)
+    main(args.power, args.cellcount, args.flightlevel)
