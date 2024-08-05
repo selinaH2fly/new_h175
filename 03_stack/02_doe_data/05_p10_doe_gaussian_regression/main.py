12f93ef958c9e0810948325b5032b7e846f09639
Wenzel Gassner <wenzel.gassner@h2fly.de>
2024-08-05 08:41:53 +0000

    geather all essential plots into Auswertung_plots_py added bug dataset and removed old "chaotic" Plot script

--- 03_stack/02_doe_data/05_p10_doe_gaussian_regression/main.py
+++ 03_stack/02_doe_data/05_p10_doe_gaussian_regression/main.py
@@ -23,18 +23,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Main script to call optimize_input_variables.py")
     parser.add_argument("-m", "--model", type=str, help="Path to the trained GPR model", default="gpr_model_cell_voltage_longrun.pth")
     parser.add_argument("-p", "--power", type=float, nargs='+', help="Power constraint for input variable optimization", default=[20,175])
-    parser.add_argument("-n", "--cellcount", type=float, nargs='+', help="Stack cell number for optimizing subject to power constraint", default=[400,400])
-    parser.add_argument("-f", "--flightlevel", type=float,  nargs='+', help="Flight level in 100x feets", default=[120, 120])
+    parser.add_argument("-n", "--cellcount", type=float, nargs='+', help="Stack cell number for optimizing subject to power constraint", default=[400,500])
+    parser.add_argument("-f", "--flightlevel", type=float,  nargs='+', help="Flight level in 100x feets", default=[0, 120])
     parser.add_argument("--mode", type=str, choices=["auto", "manual"], default="auto", help="Mode of operation: 'auto' or 'manual'")
 
     args = parser.parse_args()
-    _step_p = 10
+    _step_p = 5
     _step_c = 50
-    _step_fl = 100
+    _step_fl = 20
     #TODO: range_power is ugly deined atm. due to not starting at 0 and want to have inclusive bounds.... maybe there is a better way?
-    #range_power = np.arange(args.power[0], args.power[1] + 1, _step_p) if (args.power[1] - args.power[0]) % _step_p == 0 else np.append(np.arange(args.power[0], args.power[1], _step_p), args.power[1])
+    range_power = np.arange(args.power[0], args.power[1] + 1, _step_p) if (args.power[1] - args.power[0]) % _step_p == 0 else np.append(np.arange(args.power[0], args.power[1], _step_p), args.power[1])
     #range_power = [20,0.9*175,175] #chrs wish
-    range_power = [round(value, 1) for value in np.linspace(args.power[0], args.power[1], 12)]#with 12 we come very close to a 90% value
+    #range_power = [round(value, 1) for value in np.linspace(args.power[0], args.power[1], 12)]#with 12 we come very close to a 90% value
     
     range_cellcount = np.arange(args.cellcount[0],args.cellcount[1]+_step_c,_step_c)
     range_fl = np.arange(args.flightlevel[0],args.flightlevel[1]+_step_fl,_step_fl)
