e73edda90ae0b98186953745135456a84dc69e0b
Wenzel Gassner <wenzel.gassner@h2fly.de>
2024-08-07 13:08:15 +0000

    included turbineand eol as parameters to loop over

--- 03_stack/02_doe_data/05_p10_doe_gaussian_regression/main.py
+++ 03_stack/02_doe_data/05_p10_doe_gaussian_regression/main.py
@@ -22,23 +22,30 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description="Main script to call optimize_input_variables.py")
-    parser.add_argument("-p", "--power", type=float, nargs='+', help="Power constraint for input variable optimization", default=[20,180])
-    parser.add_argument("-n", "--cellcount", type=float, nargs='+', help="Stack cell number for optimizing subject to power constraint", default=[500,500])
+    parser.add_argument("-p", "--power", type=float, nargs='+', help="Power constraint for input variable optimization", default=[20,175])
+    parser.add_argument("-n", "--cellcount", type=float, nargs='+', help="Stack cell number for optimizing subject to power constraint", default=[400,500])
     parser.add_argument("-f", "--flightlevel", type=float,  nargs='+', help="Flight level in 100x feets", default=[120, 120])
     parser.add_argument("-t", "--turbine", type=str, choices=["true", "false"], default="true", help="Specifies whether recuperation shall be taken into account (default: True).")
     parser.add_argument("--eol", type=str, choices=["true", "false"], default="false", help="Specifies whether cell voltage is derated by a factor of 0.8 to account for end of life (default: False).")
 
     args = parser.parse_args()
     
-    _step_p = 40
+    _step_p = 60
     _step_c = 50
     _step_fl = 20
     #TODO: range_power is ugly deined atm. due to not starting at 0 and want to have inclusive bounds.... maybe there is a better way?
     range_power = np.arange(args.power[0], args.power[1] + 1, _step_p) if (args.power[1] - args.power[0]) % _step_p == 0 else np.append(np.arange(args.power[0], args.power[1], _step_p), args.power[1])
+    #range_power = np.array([20, 50, 80, 125, 150, 175])
     range_cellcount = np.arange(args.cellcount[0],args.cellcount[1]+_step_c,_step_c)
     range_fl = np.arange(args.flightlevel[0],args.flightlevel[1]+_step_fl,_step_fl)
     
-    parameters = list(itertools.product(range_power, range_cellcount, range_fl))
+    # Convert turbine and eol to boolean lists
+    range_turbine = [args.turbine.lower() == "true", args.turbine.lower() == "false"]
+    range_eol = [args.eol.lower() == "true", args.eol.lower() == "false"]
+
+    # Generate all combinations of parameters
+    parameters = list(itertools.product(range_power, range_cellcount, range_fl, range_turbine, range_eol))
+
     
     for parameter in parameters:
         
