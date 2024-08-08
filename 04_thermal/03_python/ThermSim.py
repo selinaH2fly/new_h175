8b216495ecb2a93aae433d99ed285192dc32f6b7
Felix Frey <felix.frey@h2fly.de>
2024-08-08 17:04:57 +0200

    Added preprocessing of equation system to reduce number of equations and improve convergence.

--- 04_thermal/03_python/ThermSim.py
+++ 04_thermal/03_python/ThermSim.py
@@ -6,89 +6,166 @@ import numpy as np
 
 class Circuit:
     def __init__(self):
-        self.eq_list = []
-        self.eq_list_sorted = []
-        self.var_list = []
+        self.eq_unsorted = []
+        self.eq_sorted = []
+        self.var_name = []
         self.var_init = []
         self.var_min = []
         self.var_max = []
-        self.var_unit_list = []
+        self.var_unit = []
+        self.var_removal = []
+        self.var_replacement = []
+        self.var_res = []
     
 
     def add_comp(self, comp):
-        self.eq_list.append(comp.set_eq())
+        self.eq_unsorted.append(comp.set_eq())
 
 
     def add_bc(self, bc):
-        self.eq_list.append([bc])
-    
-
-    def sort_eq(self):
-        self.eq_list = sum(self.eq_list, [])
-        
-        for eq in self.eq_list:
-            start, end = eq.split("=")
-            self.eq_list_sorted.append("(" + start + ") - (" + end + ")")
+        self.eq_unsorted.append([bc])
     
 
     def get_var(self):
-        for eq in self.eq_list_sorted:
-            eq_parsed = ast.parse(eq)
+        self.eq_unsorted = sum(self.eq_unsorted, [])
+        
+        for eq in self.eq_unsorted:
+            start, end = eq.split("=")            
+            eq_parsed = ast.parse("(%s) - (%s)"%(start, end))
+            
             for var in ast.walk(eq_parsed):
                 if isinstance(var, ast.Name):
-                    self.var_list.append(var.id)
+                    self.var_name.append(var.id)
         
-        self.var_list = sorted(set(self.var_list))
+        self.var_name = sorted(set(self.var_name))
 
-        for var in self.var_list:
+        for var in self.var_name:
             if var.startswith("p_"):
                 self.var_init.append(1.0)
                 self.var_min.append(0.0)
                 self.var_max.append(np.inf)
-                self.var_unit_list.append("bar")
+                self.var_unit.append("bar")
             elif var.startswith("T_"):
                 self.var_init.append(273.15)
                 self.var_min.append(0.0)
                 self.var_max.append(np.inf)
-                self.var_unit_list.append("K")
+                self.var_unit.append("K")
             elif var.startswith("Vdot"):
                 self.var_init.append(1.0)
                 self.var_min.append(0.0)
                 self.var_max.append(np.inf)
-                self.var_unit_list.append("l/s")
+                self.var_unit.append("l/s")
             elif var.startswith("delta_p"):
                 self.var_init.append(1.0)
-                self.var_min.append(- 1000.0)
+                self.var_min.append(- np.inf)
                 self.var_max.append(np.inf)
-                self.var_unit_list.append("bar")
-            elif var.startswith("Q"):
+                self.var_unit.append("bar")
+            elif var.startswith("Qdot"):
                 self.var_init.append(10000.0)
-                self.var_min.append(- 10000000)
+                self.var_min.append(- np.inf)
                 self.var_max.append(np.inf)
-                self.var_unit_list.append("W")
+                self.var_unit.append("W")
             elif var.startswith("nsplit"):
                 self.var_init.append(0.5)
                 self.var_min.append(0.0)
                 self.var_max.append(1.0)
-                self.var_unit_list.append("")
+                self.var_unit.append("")
             elif var.startswith("nmix"):
                 self.var_init.append(0.5)
                 self.var_min.append(0.0)
                 self.var_max.append(1.0)
-                self.var_unit_list.append("")
+                self.var_unit.append("")
             else:
                 print("ERROR! Missing definition of initialization for variable.")
     
 
+    def reduce_var(self):
+        restart = True
+
+        while restart == True:
+            restart = False
+
+            for i in range(len(self.eq_unsorted)):
+                start, end = self.eq_unsorted[i].split("=")
+                start = start.strip()
+                end = end.strip()
+
+                if start in self.var_name and end in self.var_name:
+                    var1 = start
+                    var2 = end
+                    self.eq_unsorted.pop(i)
+                    self.var_removal.append(var1)
+                    self.var_replacement.append(var2)
+
+                    for j in range(len(self.eq_unsorted)):
+                        start, end = self.eq_unsorted[j].split("=")
+                        start = start.strip()
+                        end = end.strip()
+                        start = ast.unparse(NodeRenamer(var1, var2).visit(ast.parse(start)))
+                        end = ast.unparse(NodeRenamer(var1, var2).visit(ast.parse(end)))
+                        self.eq_unsorted[j] = "%s = %s"%(start, end)
+                    
+                    for k in range(len(self.var_name)):
+                        if self.var_name[k] == var1:
+                            self.var_name.pop(k)
+                            self.var_init.pop(k)
+                            self.var_min.pop(k)
+                            self.var_max.pop(k)
+                            self.var_unit.pop(k)
+                            break
+
+                    for l in range(len(self.var_replacement)):
+                        if self.var_replacement[l] == var1:
+                            self.var_replacement[l] = var2
+
+                    restart = True
+                    break
+    
+
+    def sort_eq(self):
+        for eq in self.eq_unsorted:
+            start, end = eq.split("=")
+            self.eq_sorted.append("(" + start + ") - (" + end + ")")
+    
+
     def calculate_res(self, variables):
-        for i in range(len(self.var_list)):
-            globals()[self.var_list[i]] = variables[i]
-        self.eq_list_eval = []
+        for i in range(len(self.var_name)):
+            globals()[self.var_name[i]] = variables[i]
+        
+        self.eq_unsorted_eval = []
 
-        for eq in self.eq_list_sorted:
-            self.eq_list_eval.append(eval(eq))
+        for eq in self.eq_sorted:
+            self.eq_unsorted_eval.append(eval(eq))
 
-        return self.eq_list_eval
+        return self.eq_unsorted_eval
+    
+
+    def extend_var(self):
+        for i in range(len(self.var_removal)):
+            for j in range(len(self.var_name)):
+                if self.var_name[j] == self.var_replacement[i]:
+                    self.var_name.append(self.var_removal[i])
+                    self.var_init.append(self.var_init[j])
+                    self.var_min.append(self.var_min[j])
+                    self.var_max.append(self.var_max[j])
+                    self.var_unit.append(self.var_unit[j])
+                    self.var_res.append(self.var_res[j])
+        
+        self.var_name, self.var_init, self.var_min, self.var_max, self.var_unit, self.var_res = zip(*sorted(zip(self.var_name, self.var_init, self.var_min, self.var_max, self.var_unit, self.var_res)))
+
+
+class NodeRenamer(ast.NodeTransformer):
+    def __init__(self, var1, var2):
+        ast.NodeTransformer.__init__(self)
+        self.var1 = var1
+        self.var2 = var2
+
+
+    def visit_Name(self, node):
+        if node.id == self.var1:
+            node.id = self.var2
+
+        return node
 
 
 class Pump:
@@ -110,7 +187,7 @@ class Pump:
 
 
 class Heatsource:
-    def __init__(self, p_in, T_in, Vdot_in, p_out, T_out, Vdot_out, delta_p, Q):
+    def __init__(self, p_in, T_in, Vdot_in, p_out, T_out, Vdot_out, delta_p, Qdot):
         self.p_in = p_in
         self.T_in = T_in
         self.Vdot_in = Vdot_in
@@ -118,12 +195,12 @@ class Heatsource:
         self.T_out = T_out
         self.Vdot_out = Vdot_out
         self.delta_p = delta_p
-        self.Q = Q
+        self.Qdot = Qdot
 
 
     def set_eq(self):
         self.eq1 = "%s = %s - %s"%(self.p_in, self.p_out, self.delta_p)
-        self.eq2 = "%s = %s - %s / (%s * 4180.0)"%(self.T_in, self.T_out, self.Q, self.Vdot_in)               #TODO: implement Glysantin properties
+        self.eq2 = "%s = %s - %s / (%s * 4180.0)"%(self.T_in, self.T_out, self.Qdot, self.Vdot_in)               #TODO: implement Glysantin properties
         self.eq3 = "%s = %s"%(self.Vdot_in, self.Vdot_out)
         return[self.eq1, self.eq2, self.eq3]
 
@@ -187,13 +264,13 @@ pump1 = Pump("p_7", "T_7", "Vdot_7", "p_1", "T_1", "Vdot_1", "delta_p_pump1")
 circ.add_comp(pump1)
 splitter1 = Splitter("p_1", "T_1", "Vdot_1", "p_2", "T_2", "Vdot_2", "p_4", "T_4", "Vdot_4", "nsplit_1_splitter1", "nsplit_2_splitter1")
 circ.add_comp(splitter1)
-stack1 = Heatsource("p_2", "T_2", "Vdot_2", "p_3", "T_3", "Vdot_3", "delta_p_stack1", "Q_stack1")
+stack1 = Heatsource("p_2", "T_2", "Vdot_2", "p_3", "T_3", "Vdot_3", "delta_p_stack1", "Qdot_stack1")
 circ.add_comp(stack1)
-comp1 = Heatsource("p_4", "T_4", "Vdot_4", "p_5", "T_5", "Vdot_5", "delta_p_comp1", "Q_comp1")
+comp1 = Heatsource("p_4", "T_4", "Vdot_4", "p_5", "T_5", "Vdot_5", "delta_p_comp1", "Qdot_comp1")
 circ.add_comp(comp1)
 mixer1 = Mixer("p_3", "T_3", "Vdot_3", "p_5", "T_5", "Vdot_5", "p_6", "T_6", "Vdot_6", "nmix_1_mixer1", "nmix_2_mixer1")
 circ.add_comp(mixer1)
-rad1 = Heatsource("p_6", "T_6", "Vdot_6", "p_7", "T_7", "Vdot_7", "delta_p_rad1", "Q_rad1")
+rad1 = Heatsource("p_6", "T_6", "Vdot_6", "p_7", "T_7", "Vdot_7", "delta_p_rad1", "Qdot_rad1")
 circ.add_comp(rad1)
 
 
@@ -204,15 +281,17 @@ circ.add_bc("T_2 = 273.15 + 60.0")
 circ.add_bc("T_3 = 273.15 + 75.0")
 circ.add_bc("delta_p_stack1 = - 2.0")
 circ.add_bc("delta_p_rad1 = - 1.0")
-circ.add_bc("Q_stack1 = 100000.0")
-circ.add_bc("Q_comp1 = 5000.0")
+circ.add_bc("Qdot_stack1 = 100000.0")
+circ.add_bc("Qdot_comp1 = 5000.0")
 
 
 # Evaluation
 
-circ.sort_eq()
-
 circ.get_var()
+
+circ.reduce_var()
+
+circ.sort_eq()
 
 solution = sp.least_squares(circ.calculate_res, np.array(circ.var_init), max_nfev=100000, gtol=1e-10, bounds=(np.array(circ.var_min), np.array(circ.var_max)))
 
@@ -220,6 +299,11 @@ if solution.success == False:
     print("ERROR! No convergence could be achieved.")
     print(solution)
 else:
+    for i in range(len(circ.var_name)):
+        circ.var_res.append(solution.x[i])
+
+    circ.extend_var()
+    
     print(solution.message)
-    for i in range(len(circ.var_list)):
-        print("%20s = %12.3f %s"%(circ.var_list[i], solution.x[i], circ.var_unit_list[i]))
+    for j in range(len(circ.var_name)):
+        print("%20s = %12.3f %s"%(circ.var_name[j], circ.var_res[j], circ.var_unit[j]))
