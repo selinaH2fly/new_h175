da65ad298bfc65150ea144867050e90cb344a43d
Stefan Notter <stefan.notter@h2fly.de>
2024-08-09 08:19:52 +0200

    feat: reci pump included in optimization routine (needs debuging, probably) + some naming refactoring

--- 03_stack/02_doe_data/05_p10_doe_gaussian_regression/data_export_csv.py
+++ 03_stack/02_doe_data/05_p10_doe_gaussian_regression/data_export_csv.py
@@ -9,7 +9,7 @@ import csv
 import os
 
 def export_to_csv(feature_names, optimal_input, bounds, hydrogen_mass_flow_g_s, cell_voltage, 
-                  system_power_kW, compressor_power_kW, turbine_power_kW, stack_power_kW, power_constraint_kW, 
+                  system_power_kW, compressor_power_kW, turbine_power_kW, reci_pump_power_kW, stack_power_kW, power_constraint_kW, 
                   specified_cell_count, flight_level_100ft, filename='optimized_input_data.csv'):
     # Check if the file already exists to determine if we need to write the header
     file_exists = os.path.isfile(filename)
@@ -24,7 +24,7 @@ def export_to_csv(feature_names, optimal_input, bounds, hydrogen_mass_flow_g_s, 
             header.extend([f'{name} (Lower Bound)' for name in feature_names])
             header.extend([f'{name} (Upper Bound)' for name in feature_names])
             header.extend(['Hydrogen Consumption (g/s)', 'Cell Voltage (V)', 
-                           'System Power (kW)', 'Compressor Power (kW)', "Turbine Power (kW)", "Stack Power (kW)"])
+                           'System Power (kW)', 'Compressor Power (kW)', "Turbine Power (kW)", "Recirculation Pump Power (kW)", "Stack Power (kW)"])
             writer.writerow(header)
         
         # Write the data
@@ -33,5 +33,5 @@ def export_to_csv(feature_names, optimal_input, bounds, hydrogen_mass_flow_g_s, 
         data.extend([f'{bound[0]:.4f}' for bound in bounds])
         data.extend([f'{bound[1]:.4f}' for bound in bounds])
         data.extend([f'{hydrogen_mass_flow_g_s:.4f}', f'{cell_voltage:.4f}', 
-                     f'{system_power_kW:.2f}', f'{compressor_power_kW:.2f}', f'{turbine_power_kW:.2f}', f'{stack_power_kW:.2f}'])
+                     f'{system_power_kW:.2f}', f'{compressor_power_kW:.2f}', f'{turbine_power_kW:.2f}', f'{reci_pump_power_kW:.2f}', f'{stack_power_kW:.2f}'])
         writer.writerow(data)
