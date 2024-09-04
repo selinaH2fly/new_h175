0f497ca32969f0183aa56306d7a32df71d655505
Stefan Notter <stefan.notter@h2fly.de>
2024-09-04 13:46:12 +0200

    chore: weight estimation plots commented out in get_plots() -> will be replaced by Kate's plots when merging

--- 05_overall/05_fcs_dimensioning_model/get_plots.py
+++ 05_overall/05_fcs_dimensioning_model/get_plots.py
@@ -536,111 +536,111 @@ def H2_consumption_vs_FL(df1, markers, fl_max, saving=True, mode="eol"):
             plt.show()
             
 #PLOT: Weiht estmate wuth fit:
-def plot_weight_estimate(data, titles, colors, components_dict, components_sd_dict, markers, saving=True, mode="bol"):
-    #TODO: Change up this plot to ref system weight and use dicts of Kate:
-    # Function to perform linear regression and return the formula and R^2
-    def linear_fit(x, y):
-        x = np.array(x).reshape(-1, 1)
-        y = np.array(y)
-        model = LinearRegression().fit(x, y)
-        y_pred = model.predict(x)
-        slope = model.coef_[0]
-        intercept = model.intercept_
-        r2 = r2_score(y, y_pred)
-        return slope, intercept, r2, y_pred
+# def plot_weight_estimate(data, titles, colors, components_dict, components_sd_dict, markers, saving=True, mode="bol"):
+#     #TODO: Change up this plot to ref system weight and use dicts of Kate:
+#     # Function to perform linear regression and return the formula and R^2
+#     def linear_fit(x, y):
+#         x = np.array(x).reshape(-1, 1)
+#         y = np.array(y)
+#         model = LinearRegression().fit(x, y)
+#         y_pred = model.predict(x)
+#         slope = model.coef_[0]
+#         intercept = model.intercept_
+#         r2 = r2_score(y, y_pred)
+#         return slope, intercept, r2, y_pred
     
-    # Filter option for eol or bol plot:
-    if mode == "eol":
-        filter_mode = True
-        mode_name = "EoL"
-    elif mode == "bol":
-        filter_mode = False
-        mode_name = "BoL"
+#     # Filter option for eol or bol plot:
+#     if mode == "eol":
+#         filter_mode = True
+#         mode_name = "EoL"
+#     elif mode == "bol":
+#         filter_mode = False
+#         mode_name = "BoL"
         
-    # The x-axis points to be plotted and searched for
-    points = [20, 125, 150, 175]
+#     # The x-axis points to be plotted and searched for
+#     points = [20, 125, 150, 175]
     
-    # Define the values of cell counts numerically
-    n_values = np.array([400, 450, 500])
+#     # Define the values of cell counts numerically
+#     n_values = np.array([400, 450, 500])
     
-    # Calculate m_stack using the formula
-    m_stack_values = 0.0766 * n_values + 9.2813  # Equation for PC
+#     # Calculate m_stack using the formula
+#     m_stack_values = 0.0766 * n_values + 9.2813  # Equation for PC
     
-    # Set up the figure and axis
-    fig, ax = plt.subplots(figsize=(10, 8))
+#     # Set up the figure and axis
+#     fig, ax = plt.subplots(figsize=(10, 8))
     
-    # Create lists to store legend handles and labels
-    handles = []
-    labels = []
+#     # Create lists to store legend handles and labels
+#     handles = []
+#     labels = []
     
-    # Extract Power of components and calculate total weight for each cell count
-    for df, title, color, m_stack_value, marker in zip(data, titles, colors, m_stack_values, markers):
-        # Filter the df for eol, bol and beeing below 700 A
-        df = df[(df["eol (t/f)"] == filter_mode) & 
-                (df["current_A (Value)"] <= 700) &
-                (df["Flight Level (100x ft)"] == 120)]
+#     # Extract Power of components and calculate total weight for each cell count
+#     for df, title, color, m_stack_value, marker in zip(data, titles, colors, m_stack_values, markers):
+#         # Filter the df for eol, bol and beeing below 700 A
+#         df = df[(df["eol (t/f)"] == filter_mode) & 
+#                 (df["current_A (Value)"] <= 700) &
+#                 (df["Flight Level (100x ft)"] == 120)]
         
-        # Extract all power points from the filtered DataFrame
-        all_points = sorted(df["System Power (kW)"].unique())
-        system_weights_of_points = []
-        system_errors_of_points = []
+#         # Extract all power points from the filtered DataFrame
+#         all_points = sorted(df["System Power (kW)"].unique())
+#         system_weights_of_points = []
+#         system_errors_of_points = []
         
-        for point in all_points:
-            closest_row = df.iloc[(df["System Power (kW)"] - point).abs().argmin()]
-            total_weight = 0
-            total_error_squared = 0
-            for key, mean_value in components_dict.items():
-                power_value = closest_row[key]
-                error_value = components_sd_dict[key] * power_value
-                component_weight = power_value * mean_value
-                total_weight += component_weight
-                total_error_squared += (error_value ** 2)
-            total_weight += m_stack_value
-            total_error = np.sqrt(total_error_squared)
+#         for point in all_points:
+#             closest_row = df.iloc[(df["System Power (kW)"] - point).abs().argmin()]
+#             total_weight = 0
+#             total_error_squared = 0
+#             for key, mean_value in components_dict.items():
+#                 power_value = closest_row[key]
+#                 error_value = components_sd_dict[key] * power_value
+#                 component_weight = power_value * mean_value
+#                 total_weight += component_weight
+#                 total_error_squared += (error_value ** 2)
+#             total_weight += m_stack_value
+#             total_error = np.sqrt(total_error_squared)
             
-            system_weights_of_points.append(total_weight)
-            system_errors_of_points.append(total_error)
+#             system_weights_of_points.append(total_weight)
+#             system_errors_of_points.append(total_error)
         
-        ax.errorbar(all_points, system_weights_of_points, yerr=system_errors_of_points, fmt=marker, color=color, label=f'{title} System weight estimate',capsize=5)  # Add horizontal lines (caps) to the error bars
+#         ax.errorbar(all_points, system_weights_of_points, yerr=system_errors_of_points, fmt=marker, color=color, label=f'{title} System weight estimate',capsize=5)  # Add horizontal lines (caps) to the error bars
         
-        # Performing linear regression for each dataset
-        slope_, intercept_, r2_, y_pred_ = linear_fit(all_points, system_weights_of_points)
+#         # Performing linear regression for each dataset
+#         slope_, intercept_, r2_, y_pred_ = linear_fit(all_points, system_weights_of_points)
         
-        # Plotting the fitted line with a label for the equation
-        ax.plot(all_points, y_pred_, linestyle='--', color=color, label=f'{title} fit: y={slope_:.2f}x+{intercept_:.2f}')
+#         # Plotting the fitted line with a label for the equation
+#         ax.plot(all_points, y_pred_, linestyle='--', color=color, label=f'{title} fit: y={slope_:.2f}x+{intercept_:.2f}')
 
-        labels.append(f'{title} fit: y={slope_:.2f}x+{intercept_:.2f}')
+#         labels.append(f'{title} fit: y={slope_:.2f}x+{intercept_:.2f}')
     
-    # Adding a text box with the components included
-    components_text = "Components Included:\n" + "\n".join(key.replace("Power (kW)", "") for key in components_dict.keys())
-    ax.text(0.05, 0.95, components_text, transform=ax.transAxes, fontsize=10,
-             verticalalignment='top', bbox=dict(boxstyle="round,pad=0.3", edgecolor='black', facecolor='lightgray'))
+#     # Adding a text box with the components included
+#     components_text = "Components Included:\n" + "\n".join(key.replace("Power (kW)", "") for key in components_dict.keys())
+#     ax.text(0.05, 0.95, components_text, transform=ax.transAxes, fontsize=10,
+#              verticalalignment='top', bbox=dict(boxstyle="round,pad=0.3", edgecolor='black', facecolor='lightgray'))
     
-    # Plotting the power density line (1.25 kW/kg)
-    density = 1.25  # Power density (kW/kg)
-    density_x = np.linspace(min(points), max(points), 100)
-    density_y = density_x / density  # y = x / density
-    ax.plot(density_x, density_y, linestyle='-.', color='black')  # No label for the line
+#     # Plotting the power density line (1.25 kW/kg)
+#     density = 1.25  # Power density (kW/kg)
+#     density_x = np.linspace(min(points), max(points), 100)
+#     density_y = density_x / density  # y = x / density
+#     ax.plot(density_x, density_y, linestyle='-.', color='black')  # No label for the line
     
-    # Add annotation text without an arrow
-    annotation_x = 150  # Position annotation at x = 150 (adjust as needed)
-    annotation_y = annotation_x / density  # Calculate y based on the density line
-    ax.text(annotation_x-10, annotation_y + 10, 'Power Density: 1.25 kW/kg', fontsize=10, color='black', ha='center')
+#     # Add annotation text without an arrow
+#     annotation_x = 150  # Position annotation at x = 150 (adjust as needed)
+#     annotation_y = annotation_x / density  # Calculate y based on the density line
+#     ax.text(annotation_x-10, annotation_y + 10, 'Power Density: 1.25 kW/kg', fontsize=10, color='black', ha='center')
     
-    # Plot the legend with reordered entries
-    ax.legend(labels=labels, bbox_to_anchor=(0.5, -0.1), loc='upper center', ncol=3)
+#     # Plot the legend with reordered entries
+#     ax.legend(labels=labels, bbox_to_anchor=(0.5, -0.1), loc='upper center', ncol=3)
     
-    plt.xlabel('System Power (kW)')
-    plt.ylabel('System Weight Estimation [kg]')
-    plt.title(f'System Weight Estimation vs System Power, {mode}')
-    plt.grid(True)
-    ax.set_ylim([20, 140])
+#     plt.xlabel('System Power (kW)')
+#     plt.ylabel('System Weight Estimation [kg]')
+#     plt.title(f'System Weight Estimation vs System Power, {mode}')
+#     plt.grid(True)
+#     ax.set_ylim([20, 140])
     
-    # Save or show the plot
-    if saving:
-        plt.savefig(f"Weight_estimation_vs_power_{mode}.png", bbox_inches='tight')
-    else:
-        plt.show()
+#     # Save or show the plot
+#     if saving:
+#         plt.savefig(f"Weight_estimation_vs_power_{mode}.png", bbox_inches='tight')
+#     else:
+#         plt.show()
 
    
 #%%  
@@ -695,7 +695,9 @@ def analyze_data(_file_path1, saving=True):
     colors = [ "tab:blue", "tab:orange",  "tab:red"]
     markers= ["o", "v", "s"]
     
+    # fl_set = 0
     fl_set = 120
+
     fl_max = max(df1["Flight Level (100x ft)"])
     
     ###########PLOT: Polcurves
@@ -720,23 +722,22 @@ def analyze_data(_file_path1, saving=True):
 
     ############Plot Weight estimate
     #Weight/Power Factor
-    componentsP_dict =  {"Compressor Power (kW)":   0.63,
-                        "Turbine Power (kW)":      0.63,
-                        "Recirculation Pump Power (kW)":    7.38,
-                        "Coolant Pump Power (kW)": 4.80}
-    components_SD_dict = {"Compressor Power (kW)":   0.1,
-                        "Turbine Power (kW)":      0.1,
-                        "Recirculation Pump Power (kW)":    4.04,
-                        "Coolant Pump Power (kW)": 1.66}
+    # componentsP_dict =  {"Compressor Power (kW)":   0.63,
+    #                     "Turbine Power (kW)":      0.63,
+    #                     "Recirculation Pump Power (kW)":    7.38,
+    #                     "Coolant Pump Power (kW)": 4.80}
+    # components_SD_dict = {"Compressor Power (kW)":   0.1,
+    #                     "Turbine Power (kW)":      0.1,
+    #                     "Recirculation Pump Power (kW)":    4.04,
+    #                     "Coolant Pump Power (kW)": 1.66}
     
-    plot_weight_estimate(data, titles, colors, componentsP_dict, components_SD_dict, markers, saving=saving, mode="bol")
-    plot_weight_estimate(data, titles, colors, componentsP_dict, components_SD_dict, markers, saving=saving, mode="eol")
+    # plot_weight_estimate(data, titles, colors, componentsP_dict, components_SD_dict, markers, saving=saving, mode="bol")
+    # plot_weight_estimate(data, titles, colors, componentsP_dict, components_SD_dict, markers, saving=saving, mode="eol")
     
 # Go back to origin dir
     os.chdir("../../")
 # %%    
 
-
-analyze_data(_file_path1=r"consolidated_20-175kW_400-500_0-150ft__2\optimized_parameters_20-175kW_400-500_0-150ft.csv", saving=True)    
+# analyze_data(_file_path1=r"testing__consolidated_20-175kW_400-400_0-0ft__1\optimized_parameters_20-175kW_400-400_0-0ft.csv", saving=True)    
 
 #TODO write init:
