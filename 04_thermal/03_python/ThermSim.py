52391ff503e64be0866f6daf0f4dc4179ceede94
Julian Seiche <julian.seiche@h2fly.de>
2024-09-17 11:59:47 +0200

    Chore: Changes to thermsym because of change from list to dictionary

--- 04_thermal/03_python/ThermSim.py
+++ 04_thermal/03_python/ThermSim.py
@@ -316,85 +316,90 @@ class Circuit:
     def evaluate_big_arch(self, result_dict):
         """
         saves in 'result_dict' choosen variables in excel file. Runs script only once
-        result...dict={'Name of variabel' : ['Legend for plotting', []]}      
+        result...dict = {'Name of variabel' : ['ArchNameVar', [], 'Legend for plotting']}      
         """
         self.evaluate()
 
         for j in range(len(self.var_name)):
                 for result_name in result_dict:
-                    if self.var_name[j] == result_name:
+                    if self.var_name[j] == result_dict[result_name][0]:
                         result_dict[result_name][1].append(self.var_res[j])
 
         data_dict = {}
         for result_name in result_dict:
-            data_dict[result_dict[result_name][0]] = result_dict[result_name][1]
+            data_dict[result_dict[result_name][2]] = result_dict[result_name][1]
         data = pd.DataFrame(data_dict)
         datatoexcel = pd.ExcelWriter('results.xlsx')
         data.to_excel(datatoexcel)
         datatoexcel.close()
 
 
-    def analyse_big_arch(self, input_list, result_dict):
+    def analyse_big_arch(self, input_dict, result_dict):
         """
         analyse choosen variables over the variation of a choosen input variable. Therefore runs sript in loop several times.
         Results are plotted and saved in excelfile.                                 
-        result...dict = {'Name of variabel' : ['Legend for plotting', []]}                     
+        result...dict = {'Name of variabel' : ['ArchNameVar', [], 'Legend for plotting']}                     
         inputlist= ['Name of Variable', [...] , 'Legend for plotting']]  [...] is a List of values for variable to be invistigated
         """
         for i in range(len(self.eq_unsorted)):
-            if '%s = ' %(input_list[0]) in self.eq_unsorted[i][0]:
-                k = i
+            for name in input_dict:
+                if '%s = ' %(input_dict[name][0]) in self.eq_unsorted[i][0]:
+                    k = i
         
-        for y in input_list[1]:
-            self.eq_unsorted[k] = ['%s = %f' %(input_list[0], y)]
-            self.evaluate()
+        for name in input_dict:
+            for y in input_dict[name][1]:
+                self.eq_unsorted[k] = ['%s = %f' %(input_dict[name][0], y)]
+                self.evaluate()
 
-            for j in range(len(self.var_name)):
-                for result_name in result_dict:
-                    if self.var_name[j] == result_name:
-                        result_dict[result_name][1].append(self.var_res[j])
+                for j in range(len(self.var_name)):
+                    for result_name in result_dict:
+                        if self.var_name[j] == result_dict[result_name][0]:
+                            result_dict[result_name][1].append(self.var_res[j])
 
-            self.reset_to_startcond()
+                self.reset_to_startcond()
 
-        for result_name in result_dict:
-            plt.plot(input_list[1], result_dict[result_name][1], label=result_dict[result_name][0])
-            plt.ylabel(result_dict[result_name][0])
-            plt.xlabel(input_list[2])
-            plt.legend()
-            plt.grid()
-            plt.show()
+        for name in input_dict:
+            for result_name in result_dict:
+                plt.plot(input_dict[name][1], result_dict[result_name][1])
+                plt.ylabel(result_dict[result_name][2])
+                plt.xlabel(input_dict[name][2])
+                plt.grid()
+                plt.show()
 
         data_dict = {}
-        data_dict[input_list[0]] = input_list[1]
+        for name in input_dict:
+            data_dict[input_dict[name][2]] = input_dict[name][1]
         for result_name in result_dict:
-            data_dict[result_dict[result_name][0]] = result_dict[result_name][1]
+            data_dict[result_dict[result_name][2]] = result_dict[result_name][1]
         data = pd.DataFrame(data_dict)
         datatoexcel = pd.ExcelWriter('results.xlsx')
         data.to_excel(datatoexcel)
         datatoexcel.close()
         
 
-    def compare_big_arch(self, input_list, result_dict):
+    def compare_big_arch(self, input_dict, result_dict):
         """
         analyse choosen variables over the variation of a choosen input variable. Therefore runs sript in loop several times.
         Results are returned to be compared between the variables                                
         result...dict = {'Name of variabel' : ['Legend for plotting', []]}                     
         inputlist= ['Name of Variable', [...] , 'Legend for plotting']]  [...] is a List of values for variable to be invistigated
         """
-        for i in range(len(self.eq_unsorted)):
-            if '%s = ' %(input_list[0]) in self.eq_unsorted[i][0]:
-                k = i
-        
-        for y in input_list[1]:
-            self.eq_unsorted[k] = ['%s = %f' %(input_list[0], y)]
-            self.evaluate()
+        for name in input_dict:
+            for i in range(len(self.eq_unsorted)):
+                if '%s = ' %(input_dict[name][0]) in self.eq_unsorted[i][0]:
+                    k = i
 
-            for j in range(len(self.var_name)):
-                for result_name in result_dict:
-                    if self.var_name[j] == result_name:
-                        result_dict[result_name][1].append(self.var_res[j])
+        for name in input_dict:
+            for y in input_dict[name][1]:
+                self.eq_unsorted[k] = ['%s = %f' %(input_dict[name][0], y)]
+                self.evaluate()
 
-            self.reset_to_startcond()
+                for j in range(len(self.var_name)):
+                    for result_name in result_dict:
+                        if self.var_name[j] == result_dict[result_name][0]:
+                            result_dict[result_name][1].append(self.var_res[j])
+
+                self.reset_to_startcond()
 
         return result_dict
 
