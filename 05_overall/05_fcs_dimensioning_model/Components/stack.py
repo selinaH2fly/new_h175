06c38f48e286512fdc404f31c7f43b03b6806ea1
Wenzel Gassner <wenzel.gassner@h2fly.de>
2024-09-19 09:02:15 +0000

    feat: moved function from basic_pysics into stack and introduced calculate_heat_flux() and calculate_coolant_flow(), needs testing

--- 05_overall/05_fcs_dimensioning_model/Components/stack.py
+++ 05_overall/05_fcs_dimensioning_model/Components/stack.py
@@ -1,10 +1,16 @@
+import CoolProp.CoolProp as CP
+from parameters import Physical_Parameters
+from basic_physics import icao_atmosphere
+
 class Stack:
     """
     A class to model a fuel cell stack
     """
 
-    def __init__(self, cellcount=455, current_A=400, coolant_flow_m3_s=0.0, anode_pressure_drop_coefficients=[0, 0, 0],
-                 cooling_pressure_drop_coefficients=[0, 0, 0]):
+    def __init__(self, cellcount=455, current_A=400, cell_voltage_V = 0.7, 
+                 coolant_flow_m3_s=0.0, anode_pressure_drop_coefficients=[0, 0, 0],
+                 cooling_pressure_drop_coefficients=[0, 0, 0], 
+                 temp_coolant_in_K= 65, temp_coolant_out_K=70):
         """
         Initialize the radiator with a given pressure drop and coolant flow rate.
 
@@ -13,9 +19,13 @@ class Stack:
 
         self.cellcount = cellcount
         self.current_A = current_A
+        self.cell_voltage_V = cell_voltage_V
         self.coolant_flow_m3_s = coolant_flow_m3_s
         self.anode_pressure_drop_coefficients = anode_pressure_drop_coefficients
         self.cooling_pressure_drop_coefficients = cooling_pressure_drop_coefficients
+        #
+        self.temp_coolant_in_K = temp_coolant_in_K
+        self.temp_coolant_out_K = temp_coolant_out_K
 
     def calculate_pressure_drop_anode(self)->float:
         """
@@ -47,6 +57,37 @@ class Stack:
         # TODO: Do weight estimation here.
         # Placeholder implementation, update with actual logic
         return None
+    
+    def calculate_heat_flux(self) -> float:
+        
+        # Load the physical parameters
+        params_physics = Physical_Parameters()
+        
+        # Calculate the heat generated in the stack TODO: account for stack radiation
+        heat_flux_W = self.current_A * self.cellcount * (params_physics.hydrogen_lhv_voltage_equivalent - self.cell_voltage_V)
+        
+        return heat_flux_W 
+        
+    def calculate_coolant_flow(self, flight_level_100ft=50):
+        """
+        Compute the coolant flow rate in kg/s.
+
+        
+        """
+
+        heat_generated_W = self.calculate_heat_flux()
+
+        # Calculate the heat capacity of the coolant
+        #TODO: maybe move this to a component coolant in the future and use it in all cooled components...?
+        coolant = "INCOMP::MEG-50%" # 50% Ethylene Glycol (MEG) and 50% Water, i.e., Glysantin
+        _, pressure_ambient_Pa = icao_atmosphere(flight_level_100ft=flight_level_100ft) # Assuming coolant pressure \approx. ambient pressure
+        coolant_cp_J_kgK = CP.PropsSI('C', 'T', (self.temp_coolant_in_K + self.temp_coolant_out_K) / 2, 'P', pressure_ambient_Pa, coolant)
+
+        # Calculate the coolant flow rate
+        coolant_flow_kg_s = heat_generated_W / (coolant_cp_J_kgK * (self.temp_coolant_out_K - self.temp_coolant_in_K))
+        coolant_flow_m3_s = coolant_flow_kg_s / CP.PropsSI('D', 'T', (self.temp_coolant_in_K + self.temp_coolant_out_K) / 2, 'P', pressure_ambient_Pa, coolant)
+
+        return coolant_flow_m3_s.item()
 
 # %% Example Usage:
 
--- 05_overall/05_fcs_dimensioning_model/basic_physics.py
+++ 05_overall/05_fcs_dimensioning_model/basic_physics.py
@@ -38,29 +38,6 @@ def compute_air_mass_flow(stoichiometry, current_A, cellcount=275):
 
     return air_flow_kg_s
 
-def compute_coolant_flow(current_A, cell_voltage_V, temp_coolant_in_degC, temp_coolant_out_degC, cellcount=275, flight_level_100ft=50):
-    """
-    Compute the coolant flow rate in kg/s.
-    # TODO: move to stack component
-    """
-
-    # Load the physical parameters
-    params_physics = Physical_Parameters()
-
-    # Calculate the heat generated in the stack TODO: account for stack radiation
-    heat_generated_W = current_A * cellcount * (params_physics.hydrogen_lhv_voltage_equivalent - cell_voltage_V)
-
-    # Calculate the heat capacity of the coolant
-    coolant = "INCOMP::MEG-50%" # 50% Ethylene Glycol (MEG) and 50% Water, i.e., Glysantin
-    _, pressure_ambient_Pa = icao_atmosphere(flight_level_100ft=flight_level_100ft) # Assuming coolant pressure \approx. ambient pressure
-    coolant_cp_J_kgK = CP.PropsSI('C', 'T', (temp_coolant_in_degC + temp_coolant_out_degC) / 2 + 273.15, 'P', pressure_ambient_Pa, coolant)
-
-    # Calculate the coolant flow rate
-    coolant_flow_kg_s = heat_generated_W / (coolant_cp_J_kgK * (temp_coolant_out_degC - temp_coolant_in_degC))
-    coolant_flow_m3_s = coolant_flow_kg_s / CP.PropsSI('D', 'T', (temp_coolant_in_degC + temp_coolant_out_degC) / 2 + 273.15, 'P', pressure_ambient_Pa, coolant)
-
-    return coolant_flow_m3_s.item()
-
 #%% Example usage
 
 temperature_K, pressure_Pa = icao_atmosphere(flight_level_100ft=120)
