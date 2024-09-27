991ebd49472e5fd2050f89e157f0f4edea822aa9
Wenzel Gassner <wenzel.gassner@h2fly.de>
2024-09-27 06:23:38 +0000

    Fix: due to many points in the batch run some plots were sus, compressormap uses now all points (not only FL 120), Grid plots now only display the points from the specified powerrange.

--- 05_overall/05_fcs_dimensioning_model/Postprocessing/get_plots.py
+++ 05_overall/05_fcs_dimensioning_model/Postprocessing/get_plots.py
@@ -147,7 +147,7 @@ def analyze_data(_file_path1, saving=True):
     os.makedirs("DoE_Envelope_Evaluation", exist_ok=True)
     os.chdir("DoE_Envelope_Evaluation")
     
-    plot_optimized_parameter_DoE_envelope(df1, Optimized_DoE_data_variables, saving=saving)
+    #plot_optimized_parameter_DoE_envelope(df1, Optimized_DoE_data_variables, saving=saving)
     os.chdir("../")
 
     ##########PLOT: Optimized Operating Parameters
@@ -174,7 +174,7 @@ def analyze_data(_file_path1, saving=True):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Main script to call get_plots.py")
-    parser.add_argument("-f", "--filepath", type=str, help="path to csv file", default=r"..\TOS_V_2_1\optimized_parameters_20-175kW_400-500_0-120ft.csv")
+    parser.add_argument("-f", "--filepath", type=str, help="path to csv file", default=r"..\consolidated_20-175kW_400-500_0-120ft__2\optimized_parameters_20-175kW_400-500_0-120ft.csv")
     parser.add_argument("-s", "--saving", type=str, choices=["True", "False"], default="True", help="Whether to save plots as .png files")
     args = parser.parse_args()
     
