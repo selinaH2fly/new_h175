2fb664082a49fbbaad7710c4980af17441b8d811
Wenzel Gassner <wenzel.gassner@h2fly.de>
2024-10-10 12:37:54 +0000

    fix math mistake for the efficiency

--- 05_overall/05_fcs_dimensioning_model/Components/turbine.py
+++ 05_overall/05_fcs_dimensioning_model/Components/turbine.py
@@ -88,7 +88,7 @@ class Turbine:
         specific_heat_ratio = CP.PropsSI('C', 'T', self.temperature_in_K, 'P', self.pressure_in_Pa, 'Air') / \
             CP.PropsSI('O', 'T', self.temperature_in_K, 'P', self.pressure_in_Pa, 'Air')
         
-        temperature_out_K = self.temperature_in_K * (((self.pressure_out_Pa/self.pressure_in_Pa)**((specific_heat_ratio-1)/specific_heat_ratio)-1)/(self.isentropic_efficiency) + 1)
+        temperature_out_K = self.temperature_in_K * (((self.pressure_out_Pa/self.pressure_in_Pa)**((specific_heat_ratio-1)/specific_heat_ratio)-1)*(self.isentropic_efficiency) + 1)
         #self.temperature_out_K = temperature_out_K
         
         return temperature_out_K
