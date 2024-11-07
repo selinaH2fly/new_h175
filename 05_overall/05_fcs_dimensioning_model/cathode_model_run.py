23e89a5d3b25bde7614de2264fa85dd591b68bf6
H2FLY\debjani.ghosh <debjani.ghosh@h2fly.de>
2024-11-07 14:26:13 +0100

    chore: commented out unused variable(for now)

--- 05_overall/05_fcs_dimensioning_model/cathode_model_run.py
+++ 05_overall/05_fcs_dimensioning_model/cathode_model_run.py
@@ -19,38 +19,38 @@ class Input:
         # Pressure initialization in bar (bara)
         self.pressures_bara = {
             "PTC1": 0.63,  # Ambient pressure for FL120
-            "PTC2": 2.6,  # Pressure after compressor
-            "PTC3": 2.5,  # Pressure after intercooler, air-air, warm side
+          #  "PTC2": 2.6,  # Pressure after compressor
+           # "PTC3": 2.5,  # Pressure after intercooler, air-air, warm side
             "PTC4": 2.4,  # Pressure after intercooler, air-liq
-            "PTC5": 2.4,  # Pressure after air filter
-            "PTC6": 2.3,  # Pressure before valve
-            "PTC7": 2.3,  # Pressure after humidifier, dry
-            "PTC8": 2.1,  # Pressure before stack
-            "PTC9": 2.1,  # Pressure after stack
-            "PTC10": 2.1, # Pressure after humidifier,wet
-            "PTC11": 1.8, # Pressure before intercooler, air-air, cold side
-            "PTC13": 1.8, # Pressure after water separator
-            "PTC12": 1.7, # Pressure after intercooler, air-air, cold side
-            "PTC14": 1.7  # Pressure after turbine
+          #  "PTC5": 2.4,  # Pressure after air filter
+          #  "PTC6": 2.3,  # Pressure before valve
+            "PTC7": 3,  # Pressure after humidifier, dry
+            "PTC8": 3,  # Pressure before stack
+            "PTC9": 2.7,  # Pressure after stack
+           # "PTC10": 2.1, # Pressure after humidifier,wet
+           # "PTC11": 1.8, # Pressure before intercooler, air-air, cold side
+            #"PTC13": 1.8, # Pressure after water separator
+           # "PTC12": 1.7, # Pressure after intercooler, air-air, cold side
+            "PTC14": 0.64  # Pressure after turbine
 
         }
 
         # Temperature initialization in degrees Celsius
         self.temperatures_degC = {
             "TTC1": 6.3,     # Ambient temperature
-            "TTC2": 80,    # Temperature after compressor
-            "TTC3": 80,     # Temperature after intercooler, air-air
-            "TTC4": 80,     # Temperature after intercooler, air-liq
-            "TTC7": 80,   # Temperature after humidifier, to be calculated
-            "TTC5": 80,  # Temperature before humidifier, to be calculated
-            "TTC6": 80,  # Temperature before valve, to be calculated
+            #"TTC2": 80,    # Temperature after compressor
+           # "TTC3": 80,     # Temperature after intercooler, air-air
+           # "TTC4": 80,     # Temperature after intercooler, air-liq
+           # "TTC7": 80,   # Temperature after humidifier, to be calculated
+           # "TTC5": 80,  # Temperature before humidifier, to be calculated
+          #  "TTC6": 80,  # Temperature before valve, to be calculated
             "TTC8": 78,  # Temperature before stack
             "TTC9": 82,   # Temperature after stack
             "TTC10": 77,  # Temperature after humidifier,wet
-            "TTC11": 77,  # Temperature before intercooler, air-air, cold side
-            "TTC13": 77,  # Temperature after water separator
-            "TTC12": 77,  # Temperature after intercooler, air-air, cold side
-            "TTC14": 77,  # Temperature after turbine
+          #  "TTC11": 77,  # Temperature before intercooler, air-air, cold side
+          #  "TTC13": 77,  # Temperature after water separator
+           # "TTC12": 77,  # Temperature after intercooler, air-air, cold side
+         #   "TTC14": 77,  # Temperature after turbine
             "T_cool": 60    # Temperature of coolant input to IC air-liq
         }
 
